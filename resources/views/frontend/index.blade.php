9ca5b9c12015345c3eebf84cb5581af359a4abca
Ariph-off <arifh070805@gmail.com>
2023-08-06 13:00:30 +0700

    landingpage commit

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,78 +1,183 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter&family=Poppins&display=swap');
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
+
+        * {
+            margin: 0px;
+            padding: 0px;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
+        }
+
+        :root {
+            --main-color: #f0b30f;
+        }
+
+        .theme-text {
+            color: var(--main-color);
+        }
+
+        .act {
+            color: var(--main-color);
+            border-bottom: 2px solid var(--main-color);
+        }
+
+        .navbar ul li a:hover {
+            color: var(--main-color);
+            border-bottom: 2px solid var(--main-color);
+        }
+
+        .header {
+            background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("img/extraa.png");
+            width: 100%;
+            height: 100vh;
+            background-position: center;
+            background-size: cover;
+            position: relative;
+        }
+
+        .wave {
+            position: absolute;
+            bottom: 0px;
+        }
+
+        .midlle {
+            height: 80vh;
+            width: 50%;
+            display: flex;
+            justify-content: start;
+            align-items: center;
+        }
+
+        .midlle h1 {
+            font-size: 60px;
+        }
+
+        @media screen and (max-width: 425px) {
+            .midlle h1 {
+                font-size: 45px;
+            }
+
+            .text-copyright {
+                font-size: 10px;
+            }
+
+            .text-warning {
+                font-size: 15px;
+            }
+        }
+
+        @media screen and (max-width: 320px) {
+            .midlle h1 {
+                font-size: 45px;
+            }
+
+            .text-copyright {
+                font-size: 10px;
+            }
+
+            .text-warning {
+                font-size: 15px;
+            }
+        }
+    </style>
+
+    <title>EskulKuy</title>
+</head>
+
+<body>
     <section class="header">
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-dark ">
-  <div class="container-fluid">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-egg-fried" viewBox="0 0 16 16">
-        <path d="M8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-        <path d="M13.997 5.17a5 5 0 0 0-8.101-4.09A5 5 0 0 0 1.28 9.342a5 5 0 0 0 8.336 5.109 3.5 3.5 0 0 0 5.201-4.065 3.001 3.001 0 0 0-.822-5.216zm-1-.034a1 1 0 0 0 .668.977 2.001 2.001 0 0 1 .547 3.478 1 1 0 0 0-.341 1.113 2.5 2.5 0 0 1-3.715 2.905 1 1 0 0 0-1.262.152 4 4 0 0 1-6.67-4.087 1 1 0 0 0-.2-1 4 4 0 0 1 3.693-6.61 1 1 0 0 0 .8-.2 4 4 0 0 1 6.48 3.273z"/>
-      </svg>
-    <a class="navbar-brand" href="#"> &nbsp;<strong>KULINER</strong> NUSANTARA</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link act" aria-current="page" href="">Beranda</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="https://www.instagram.com/kuliner.nusantara/">Hubungi Kami</a>
-          </li>
-    </div>
-  </div>
+                <div class="container-fluid">
+                    <svg xmlns="kuli" width="16" height="16" fill="#fff" class="bi bi-egg-fried"
+                        viewBox="0 0 16 16">
+                        <path d="M8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                        <path
+                            d="M13.997 5.17a5 5 0 0 0-8.101-4.09A5 5 0 0 0 1.28 9.342a5 5 0 0 0 8.336 5.109 3.5 3.5 0 0 0 5.201-4.065 3.001 3.001 0 0 0-.822-5.216zm-1-.034a1 1 0 0 0 .668.977 2.001 2.001 0 0 1 .547 3.478 1 1 0 0 0-.341 1.113 2.5 2.5 0 0 1-3.715 2.905 1 1 0 0 0-1.262.152 4 4 0 0 1-6.67-4.087 1 1 0 0 0-.2-1 4 4 0 0 1 3.693-6.61 1 1 0 0 0 .8-.2 4 4 0 0 1 6.48 3.273z" />
+                    </svg>
+                    <a class="navbar-brand" href="#"> &nbsp;<strong>EKSTRA</strong>SMAKENSA</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link act" aria-current="page" href="">Beranda</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://www.instagram.com/kuliner.nusantara/">Hubungi Kami</a>
+                            </li>
+                    </div>
+                </div>
 
-</nav>
+            </nav>
             <!--navbar ends here -->
             <div class="midlle">
-                <h1 class="text-white fw-bold display-3">Membantu anda  menemukan<br> <span class="theme-text">makanan yang lebih baik.</span></h1>
+                <h1 class="text-white fw-bold display-3">Membantu anda menemukan<br> <span class="theme-text">ekstra
+                        yang lebih baik.</span></h1>
+                       
             </div>
         </div>
-        <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f0b30f" fill-opacity="1" d="M0,128L60,149.3C120,171,240,213,360,218.7C480,224,600,192,720,197.3C840,203,960,245,1080,234.7C1200,224,1320,160,1380,128L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-      </section>
+        <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#f0b30f" fill-opacity="1"
+                d="M0,128L60,149.3C120,171,240,213,360,218.7C480,224,600,192,720,197.3C840,203,960,245,1080,234.7C1200,224,1320,160,1380,128L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
+            </path>
+        </svg>
+    </section>
 
-      <footer class="bg-dark text-white pt-4 pb-4">
+    <footer class="bg-dark text-white pt-4 pb-4">
         <div class="content text-center text-md-left">
-          <div class="row text-center text-md-left">
-            <div class="col-md-3 col-lg3 col-xl-3 mx-auto mt-3">
-              <h5 class="text-uppercase mb-4 font-weight-bold text-light-emphasis" > <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-egg-fried" viewBox="0 0 16 16">
-                <path d="M8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                <path d="M13.997 5.17a5 5 0 0 0-8.101-4.09A5 5 0 0 0 1.28 9.342a5 5 0 0 0 8.336 5.109 3.5 3.5 0 0 0 5.201-4.065 3.001 3.001 0 0 0-.822-5.216zm-1-.034a1 1 0 0 0 .668.977 2.001 2.001 0 0 1 .547 3.478 1 1 0 0 0-.341 1.113 2.5 2.5 0 0 1-3.715 2.905 1 1 0 0 0-1.262.152 4 4 0 0 1-6.67-4.087 1 1 0 0 0-.2-1 4 4 0 0 1 3.693-6.61 1 1 0 0 0 .8-.2 4 4 0 0 1 6.48 3.273z"/>
-              </svg>  <strong>Kuliner</strong> Nusantara</h5>
-              <p class="text-warning">Kumpulan dari berbagai makanan yang berasal dari berbagai daerah di nusantara.</p>
-            </div>
-            <hr class="mb-4">
-            <div class="row align-items-center">
-              <div class="col md-7 col-lg-15">
-                <p class="">Copyright &copy All right reserved by: KULINER NUSANTARA.
-                </p>
-              </div>
-            </div>
+            <div class="row text-center text-md-left">
+                <div class="col-md-3 col-lg3 col-xl-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-light-emphasis"> <svg
+                            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff"
+                            class="bi bi-egg-fried" viewBox="0 0 16 16">
+                            <path d="M8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                            <path
+                                d="M13.997 5.17a5 5 0 0 0-8.101-4.09A5 5 0 0 0 1.28 9.342a5 5 0 0 0 8.336 5.109 3.5 3.5 0 0 0 5.201-4.065 3.001 3.001 0 0 0-.822-5.216zm-1-.034a1 1 0 0 0 .668.977 2.001 2.001 0 0 1 .547 3.478 1 1 0 0 0-.341 1.113 2.5 2.5 0 0 1-3.715 2.905 1 1 0 0 0-1.262.152 4 4 0 0 1-6.67-4.087 1 1 0 0 0-.2-1 4 4 0 0 1 3.693-6.61 1 1 0 0 0 .8-.2 4 4 0 0 1 6.48 3.273z" />
+                        </svg> <strong>Kuliner</strong> Nusantara</h5>
+                    <p class="text-warning">Yuk bergabung di ekstrakurikuler SMKN 1 Bondowoso.</p>
+                </div>
+                <hr class="mb-4">
+                <div class="row align-items-center">
+                    <div class="col md-7 col-lg-15">
+                        <p class="text-copyright">Copyright &copy All right reserved by: KULINER NUSANTARA.
+                        </p>
+                    </div>
+                </div>
 
-          </div>
+            </div>
         </div>
-      </footer>
+    </footer>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
     -->
-  </body>
+</body>
+
 </html>
