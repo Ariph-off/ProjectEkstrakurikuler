8f48a0378ab186e66531e457de905e7ba60de3c0
Ariph-off <arifh070805@gmail.com>
2023-08-07 08:44:55 +0700

    update landing

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -14,10 +14,16 @@
 
 
         * {
-            margin: 0px;
-            padding: 0px;
+            /* width: 100%; */
+            margin: 0;
+            padding: 0;
             box-sizing: border-box;
             font-family: 'Poppins', sans-serif;
+        }
+
+        body{
+            width: 100%;
+            height: 100vh;
         }
 
         :root {
