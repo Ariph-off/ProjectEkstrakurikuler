42872f3fa7c6cc12c597369dd6bb52ab35f3155c
Ariph-off <arifh070805@gmail.com>
2023-08-08 22:33:55 +0700

    update landing page

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -21,10 +21,12 @@
             font-family: 'Poppins', sans-serif;
         }
 
-        body{
+        body {
             width: 100%;
             height: 100vh;
         }
+
+
 
         :root {
             --main-color: #f0b30f;
@@ -51,6 +53,7 @@
             background-position: center;
             background-size: cover;
             position: relative;
+            margin: 0;
         }
 
         .wave {
@@ -59,18 +62,73 @@
         }
 
         .midlle {
-            height: 80vh;
+            margin-top: 70px;
+            height: 60vh;
             width: 50%;
             display: flex;
             justify-content: start;
             align-items: center;
+
         }
 
         .midlle h1 {
             font-size: 60px;
         }
 
+        .btn {
+            position: relative;
+            font-size: 17px;
+            text-transform: uppercase;
+            text-decoration: none;
+            padding: .5em 1.5em;
+            display: inline-block;
+            border-radius: 6em;
+            transition: all .2s;
+            border: none;
+            /* font-family: inherit; */
+            letter-spacing: 3px;
+            font-weight: 700;
+            color: black;
+            background-color: #f0b30f;
+        }
+
+        .btn:hover {
+            transform: translateY(-3px);
+            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+        }
+
+        .btn:active {
+            transform: translateY(-1px);
+            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+        }
+
+        .btn::after {
+            content: "";
+            display: inline-block;
+            height: 100%;
+            width: 100%;
+            border-radius: 100px;
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: -1;
+            transition: all .4s;
+        }
+
+        .btn::after {
+            background-color: #f0b30f;
+        }
+
+        .btn:hover::after {
+            transform: scaleX(1.4) scaleY(1.6);
+            opacity: 0;
+        }
+
         @media screen and (max-width: 425px) {
+            .midlle{
+                height: 40vh;
+                margin-top: 180px;
+            }
             .midlle h1 {
                 font-size: 45px;
             }
@@ -99,6 +157,8 @@
         }
     </style>
 
+
+
     <title>EskulKuy</title>
 </head>
 
@@ -107,12 +167,6 @@
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-dark ">
                 <div class="container-fluid">
-                    <svg xmlns="kuli" width="16" height="16" fill="#fff" class="bi bi-egg-fried"
-                        viewBox="0 0 16 16">
-                        <path d="M8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                        <path
-                            d="M13.997 5.17a5 5 0 0 0-8.101-4.09A5 5 0 0 0 1.28 9.342a5 5 0 0 0 8.336 5.109 3.5 3.5 0 0 0 5.201-4.065 3.001 3.001 0 0 0-.822-5.216zm-1-.034a1 1 0 0 0 .668.977 2.001 2.001 0 0 1 .547 3.478 1 1 0 0 0-.341 1.113 2.5 2.5 0 0 1-3.715 2.905 1 1 0 0 0-1.262.152 4 4 0 0 1-6.67-4.087 1 1 0 0 0-.2-1 4 4 0 0 1 3.693-6.61 1 1 0 0 0 .8-.2 4 4 0 0 1 6.48 3.273z" />
-                    </svg>
                     <a class="navbar-brand" href="#"> &nbsp;<strong>EKSTRA</strong>SMAKENSA</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -122,7 +176,7 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link act" aria-current="page" href="">Beranda</a>
+                                <a class="nav-link act" aria-current="page" href="">Login (admin)</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="https://www.instagram.com/kuliner.nusantara/">Hubungi Kami</a>
@@ -133,15 +187,20 @@
             </nav>
             <!--navbar ends here -->
             <div class="midlle">
-                <h1 class="text-white fw-bold display-3">Membantu anda menemukan<br> <span class="theme-text">ekstra
+                <h1 class="tulisan text-white fw-bold display-3">Membantu anda menemukan <br> <span
+                        class="theme-text">ekstra
                         yang lebih baik.</span></h1>
-                       
+
+            </div>
+            <div class="selengkapnya">
+                <button class="btn"> Selengkapnya
+                </button>
             </div>
         </div>
-        <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        {{-- <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path fill="#f0b30f" fill-opacity="1"
                 d="M0,128L60,149.3C120,171,240,213,360,218.7C480,224,600,192,720,197.3C840,203,960,245,1080,234.7C1200,224,1320,160,1380,128L1440,96L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
-            </path>
+            </path> --}}
         </svg>
     </section>
 
@@ -149,19 +208,15 @@
         <div class="content text-center text-md-left">
             <div class="row text-center text-md-left">
                 <div class="col-md-3 col-lg3 col-xl-3 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold text-light-emphasis"> <svg
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff"
-                            class="bi bi-egg-fried" viewBox="0 0 16 16">
-                            <path d="M8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                            <path
-                                d="M13.997 5.17a5 5 0 0 0-8.101-4.09A5 5 0 0 0 1.28 9.342a5 5 0 0 0 8.336 5.109 3.5 3.5 0 0 0 5.201-4.065 3.001 3.001 0 0 0-.822-5.216zm-1-.034a1 1 0 0 0 .668.977 2.001 2.001 0 0 1 .547 3.478 1 1 0 0 0-.341 1.113 2.5 2.5 0 0 1-3.715 2.905 1 1 0 0 0-1.262.152 4 4 0 0 1-6.67-4.087 1 1 0 0 0-.2-1 4 4 0 0 1 3.693-6.61 1 1 0 0 0 .8-.2 4 4 0 0 1 6.48 3.273z" />
-                        </svg> <strong>Kuliner</strong> Nusantara</h5>
+                    <h5 class="text-uppercase mb-4 font-weight-bold text-light-emphasis">
+                        <strong>Ekstra</strong>Smakensa
+                    </h5>
                     <p class="text-warning">Yuk bergabung di ekstrakurikuler SMKN 1 Bondowoso.</p>
                 </div>
                 <hr class="mb-4">
                 <div class="row align-items-center">
                     <div class="col md-7 col-lg-15">
-                        <p class="text-copyright">Copyright &copy All right reserved by: KULINER NUSANTARA.
+                        <p class="text-copyright">Copyright &copy All right reserved by: EkstraSmakensa.
                         </p>
                     </div>
                 </div>
@@ -169,6 +224,8 @@
             </div>
         </div>
     </footer>
+
+
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
