033f5c664773f0ce4304d77f612e6a72bfa86d35
luqmanhakim123 <114430708+luqmanhakim123@users.noreply.github.com>
2023-08-09 12:56:35 +0700

    dashboard

--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -1,12 +1,12 @@
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+  <aside class="main-sidebar sidebar-dark-primary">
+      <!-- Brand Logo -->
+      <a href="index3.html" class="brand-link">
+          {{-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> --}}
+          <span class="brand-text font-weight-light">EkstraSmakensa</span>
+      </a>
 
-    {{-- <!-- Sidebar -->
+      {{-- <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
@@ -20,26 +20,70 @@
 
       <!-- SidebarSearch Form -->
       <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
+          <div class="input-group" data-widget="sidebar-search">
+              <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                  <button class="btn btn-sidebar">
+                      <i class="fas fa-search fa-fw"></i>
+                  </button>
+              </div>
           </div>
-        </div>
       </div>
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+              data-accordion="false">
+              <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
+              <a href="" class="nav-link">
+                  <i class="fa-solid fa-book-atlas" style="color: #ffffff;"></i>
+                  {{-- <i class="nav-icon far fa-book-atlas"></i> --}}
+                  <p>
+                      Daftar Hadir
+                  </p>
+              </a>
+              <a href="" class="nav-link">
+                  <i class="fa-solid fa-book-atlas" style="color: #ffffff;"></i>
+                  {{-- <i class="nav-icon far fa-book-atlas"></i> --}}
+                  <p>
+                      Data Siswa
+                  </p>
+              </a>
+              <a href="" class="nav-link">
+                  <i class="fa-solid fa-book-atlas" style="color: #ffffff;"></i>
+                  {{-- <i class="nav-icon far fa-book-atlas"></i> --}}
+                  <p>
+                      Pembina
+                  </p>
+              </a>
+              <a href="" class="nav-link">
+                  <i class="fa-solid fa-book-atlas" style="color: #ffffff;"></i>
+                  {{-- <i class="nav-icon far fa-book-atlas"></i> --}}
+                  <p>
+                      Pelatih
+                  </p>
+              </a>
+              <a href="" class="nav-link">
+                  <i class="fa-solid fa-book-atlas" style="color: #ffffff;"></i>
+                  {{-- <i class="nav-icon far fa-book-atlas"></i> --}}
+                  <p>
+                      Jadwal
+                  </p>
+              </a>
+              <a href="" class="nav-link">
+                  <i class="fa-solid fa-book-atlas" style="color: #ffffff;"></i>
+                  {{-- <i class="nav-icon far fa-book-atlas"></i> --}}
+                  <p>
+                      Logout
+                  </p>
+              </a>
+
+              {{-- <li class="nav-item menu-open">
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Dashboard
+                Ekstrakulikuler
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
@@ -665,10 +709,10 @@
               <i class="nav-icon far fa-circle text-info"></i>
               <p>Informational</p>
             </a>
-          </li>
-        </ul>
+          </li> --}}
+          </ul>
       </nav>
       <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
+      </div>
+      <!-- /.sidebar -->
   </aside>
