7c5243bda6d74b4c8d6cf960154675fc1065948a
Yoanre <yoanrelouis2@gmail.com>
2023-08-28 07:54:52 +0700

    migration

---
+++ database/migrations/2023_08_26_045501_create_pelatih_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pelatih', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_pelatih');
+            $table->integer('id_ekstra');
+            $table->integer('no_ho');
+            $table->string('alamat');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pelatih');
+    }
+};
