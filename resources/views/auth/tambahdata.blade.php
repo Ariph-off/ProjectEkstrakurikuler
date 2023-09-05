f9ba01804ec8f3003725f3de089c5bcb1be420f9
RenoMujio <114579431+RenoMujio@users.noreply.github.com>
2023-09-05 16:29:41 +0700

    pelatih

---
+++ resources/views/auth/tambahdata.blade.php
@@ -0,0 +1,26 @@
+@extends('backend/panel')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <div class="container">
+
+        <button type="button" class="btn btn-success mt-3"><i class="fas fa-plus"></i>Tambah</button>
+
+            
+        </div>
+
+        
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+    </section>
+    <!-- right col -->
+    </div>
+    <!-- /.row (main row) -->
+    </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
